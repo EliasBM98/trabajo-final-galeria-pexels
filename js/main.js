920668bea80f553cafce204045fd63c7df03363e
Miguel Martinez <mamg1603@gmail.com>
2024-10-11 09:26:35 +0200

    pruebas imagen

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    const urlBase="https://api.pexels.com/v1";
+
+const contenedorCategorias=document.querySelector("#coleccionCategorias");
+
+const urlBase="https://api.pexels.com/v1";
+const urlInicio="curated?page=1&per_page=3";
+
+    //Conexion a la API, recibiendo las diferentes urls
     const conexion=async(url,method='GET')=>{
         try{
             const respuesta=await fetch(`${urlBase}/${url}`,{
                 method:method,
                 headers:{
                     Authorization:"5GpfihdKfeSMosPoFNyEMnrJu1Zq9mAMRnSSkqSaSdnigbKshpahrnqK"
-                }
-                
+                }  
             })
             if(respuesta.ok){
                 const data =await respuesta.json();
@@ -19,14 +24,30 @@ document.addEventListener("DOMContentLoaded",()=>{
             throw(error.message);
         }
     }
-    const pintarTabla=async (busqueda)=>{
+
+    //funcion para 
+    const pintarTabla=async (item)=>{
         try{
-            const recibido=`search?query=${busqueda}`;
-            const res = await conexion(recibido);
-            console.log({res});
+            //const recibido=`search?query=${busqueda}`;
+            //pintarTabla("Ocean")
+            //const res = await conexion(recibido);
+
+            const res = await conexion(item);
+
+            //res.photos[0].src.large
+            const aux= res.photos;
+            console.log(aux[0].src.landscape);
+            console.log(aux[1].src.landscape);
+            console.log(aux[2].src.landscape);
+            //console.log({res});
+
+            
+           
         }catch(error){
             console.log(error.message);
         }
     }
-    pintarTabla("Ocean");
+
+    pintarTabla(urlInicio);
+
     })//LOAD
